6713bac90d76ee41a171232f8c293063db6a1ea1
Chocho Acer-Windows <chochosf@gmail.com>
2016-02-15 20:29:58 +0200

    vtori

--- InvoiceGenerator/invoice.py
+++ InvoiceGenerator/invoice.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+#proba
 from tkinter import *
 from tkinter.messagebox import *
 from reportlab.pdfgen import canvas
@@ -11,7 +11,7 @@ import os
 
 
 pdfmetrics.registerFont(TTFont('Arial', 'arialbi.ttf'))
-fields = ['Име на фирма (Получател)', 'Град/Село*', 'Адрес:*', 'Идент. No:*', 'Материално Отговорно Лице (МОЛ)*', 'Име на файла(без разширението)']
+fields = ['Р�РјРµ РЅР° С„РёСЂРјР° (РџРѕР»СѓС‡Р°С‚РµР»)', 'Р“СЂР°Рґ/РЎРµР»Рѕ*', 'РђРґСЂРµСЃ:*', 'Р�РґРµРЅС‚. No:*', 'РњР°С‚РµСЂРёР°Р»РЅРѕ РћС‚РіРѕРІРѕСЂРЅРѕ Р›РёС†Рµ (РњРћР›)*', 'Р�РјРµ РЅР° С„Р°Р№Р»Р°(Р±РµР· СЂР°Р·С€РёСЂРµРЅРёРµС‚Рѕ)']
 stringvars = []
 D = {}
 root = Tk()
@@ -21,7 +21,7 @@ def fetch():
     coords =  0
     filename = fields[-1]
     if D[filename].get() == '':
-        showwarning('Грешка', 'Не сте въвели име на файла')
+        showwarning('Р“СЂРµС€РєР°', 'РќРµ СЃС‚Рµ РІСЉРІРµР»Рё РёРјРµ РЅР° С„Р°Р№Р»Р°')
         return  
     else:
         pdfnum =  D[filename].get()   
@@ -31,14 +31,14 @@ def fetch():
     
     print(fullpathtofile)
     if os.path.isfile(fullpathtofile):
-        showwarning('Грешка', 'Файлът вече съществува: ' + ime)
+        showwarning('Р“СЂРµС€РєР°', 'Р¤Р°Р№Р»СЉС‚ РІРµС‡Рµ СЃСЉС‰РµСЃС‚РІСѓРІР°: ' + ime)
         return
     
     c.setFont("Arial", 14)        
          
     for key in list(D.keys()):       
         if D[key].get() == '':
-            showwarning('Грешка', 'Моля, попълнете полето:' + key)
+            showwarning('Р“СЂРµС€РєР°', 'РњРѕР»СЏ, РїРѕРїСЉР»РЅРµС‚Рµ РїРѕР»РµС‚Рѕ:' + key)
             return
         x = D[key].get()
         c.drawString(coords, 50, x.encode('utf-8'))
@@ -54,7 +54,7 @@ for field in fields:
     ent.pack(side=TOP)
     D[field] = var
     #stringvars.append(D[field])
-button = Button(root, text='Генерирай фактура', command=fetch)
+button = Button(root, text='Р“РµРЅРµСЂРёСЂР°Р№ С„Р°РєС‚СѓСЂР°', command=fetch)
 button.pack(side=TOP)
 
 frm.pack()
